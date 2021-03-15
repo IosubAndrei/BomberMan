92f3ffe66609941e295867e24eb1973f1ead9304
Radu <58985887+Radu25@users.noreply.github.com>
2021-03-15 09:09:41 +0200

    Added Interface Class

---
+++ 3_Solution/Bomber/Bomber/Singleton.cpp
@@ -0,0 +1,33 @@
+#include "Singleton.h"
+
+Singleton* Singleton::mpInstance = nullptr;
+
+
+Singleton& Singleton::GetInstance()
+{
+	if (mpInstance == nullptr)
+		mpInstance = new Singleton;
+	return *mpInstance;
+}
+
+Singleton* Singleton::GetInstancePtr(int v)
+{
+	if (mpInstance == nullptr)
+		mpInstance = new Singleton(v);
+	else
+		mpInstance->val = v;
+	return mpInstance;
+}
+
+void Singleton::DestroyInstance()
+{
+	if (mpInstance == nullptr)
+		return;
+	delete mpInstance;
+	mpInstance = nullptr;
+}
+
+void Singleton::createEvent(string Event)
+{
+	cout << "Evenimentul- " << Event << endl;
+}
