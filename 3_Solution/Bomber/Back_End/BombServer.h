85a2853b7ec9a1814debb6676b32c700a51d7ac8
IosubAndrei <iosubandrei2010@yahoo.com>
2021-04-08 20:54:12 +0300

    Beggining of Server Idea.

---
+++ 3_Solution/Bomber/Back_End/BombServer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "GameObject.h"
+class BombServer :
+    public GameObject
+{
+private:
+    int m_explosionSize;
+
+public:
+    BombServer(sf::Vector2f startingPosition, int explosionSize);
+    int getExplosionSize() const;
+};
+
---
+++ 3_Solution/Bomber/Back_End/PlayerServer.cpp
@@ -0,0 +1,9 @@
+#include "PlayerServer.h"
+#include "Utilities.h"
+#include "ServerMessageType.h"
+#include "ServerMessages.h"
+#include <iostream>
+#include <algorithm>
+#include <assert.h>
+
+
